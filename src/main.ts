fffd9fca84c3f1eb5c9cc683785c7f5890a3ee0c
Ronen Miller <ronen.miller1@gmail.com>
2024-01-09 17:31:20 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+      .setTitle('Equity Grants')
+      .setDescription('The Equity Grant Management API Description')
+      .setVersion('1.0')
+      .addTag('equity-grant')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
